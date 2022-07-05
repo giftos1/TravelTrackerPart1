14da1be935b2982a030487ea2ca4013146462eed
Giftos1 <giftsydney676@gmail.com>
2022-07-05 19:39:20 +1000

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 """
-Replace the contents of this module docstring with your own details
 Name:Gift Sydney Ogingo
-Date started:30/08/2020
+Date started:5/7/2022
 GitHub URL:https://github.com/giftos1/TravelTrackerProject
 """
 
 
 def main():
+
     main()
