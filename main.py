0a3bf407c92a3f8e4977aefbbb1b3d3e0bd6df38
Giftos1 <giftsydney676@gmail.com>
2022-07-14 16:13:04 +1000

    restructure get_max_name_length function check for error in menu input

--- main.py
+++ main.py
@@ -25,38 +25,49 @@ def main():
 
     print(len(places), "places loaded from", FILENAME)
 
+    letters_input = ["l", "a", "m", "q"]
+    menu_input = menu()
+    while menu_input != "q":
+        if menu_input == "l":
+            get_max_name_length(places)
+            menu_input = menu()
+
+        # check if the wrong letter has been typed and return the menu if true
+        elif menu_input not in letters_input:
+            print("Invalid menu choice")
+            menu_input = menu()
+
+    print("Have a nice day :)")  # display message when user chooses q
+
+
+def menu():
     menu_input = input("Menu:\n"
                        "L - List Places\n"
                        "A - Add new place\n"
                        "M - Mark a place as visited\n"
                        "Q - Quit").lower()
+    return menu_input
 
-    get_max_name_length(menu_input, places)
 
-
-def get_max_name_length(choice_input, places):
+def get_max_name_length(places):
     """get maximum length of city and country name and call display_formatted_list"""
-    while choice_input:
-        if choice_input == "l":
 
-            city_names = []
-            country_names = []
+    city_names = []
+    country_names = []
 
-            # add the name of each city and country to respective lists
-            for each_place in places:
-                city_name = each_place[0].strip("\n")
-                city_names.append(city_name)
+    # add the name of each city and country to respective lists
+    for each_place in places:
+        city_name = each_place[0].strip("\n")
+        city_names.append(city_name)
 
-                country_name = each_place[1].strip("\n")
-                country_names.append(country_name)
+        country_name = each_place[1].strip("\n")
+        country_names.append(country_name)
 
-            # get the name of city and country with the maximum string length from respective lists
-            max_country_length = len(max(country_names, key=len))
-            max_city_length = len(max(city_names, key=len))
+    # get the name of city and country with the maximum string length from respective lists
+    max_country_length = len(max(country_names, key=len))
+    max_city_length = len(max(city_names, key=len))
 
-            display_formatted_list(max_city_length, max_country_length, places)
-
-        break
+    display_formatted_list(max_city_length, max_country_length, places)
 
 
 def display_formatted_list(max_city_length, max_country_length, places):
@@ -71,6 +82,7 @@ def display_formatted_list(max_city_length, max_country_length, places):
         # display a dynamic lined up list based on longest city and country name.
         if len(place[0]) != max_city_length and len(place[1]) != max_country_length:
             # check if place is unvisited(n) and add a star(*) before the number if true
+            # count the number of unvisited places
             if "n" in place[3]:
                 unvisited_count += 1
                 print(f"*{count}.", place[0], "{:{}}in".format("", additional_city_space), place[1],
