2dc7cae4fdeb4632966f844ce6ec0c52ef6a398d
Giftos1 <giftsydney676@gmail.com>
2022-07-31 16:49:02 +1000

    check for wrong input(blank spaces) enable user to convert unvisited place to visited write new added place to places file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ GitHub URL:https://github.com/giftos1/TravelTrackerProject
 from operator import itemgetter
 
 FILENAME = 'places.csv'
+NOT_VISITED = "n"
 
 
 def main():
@@ -22,34 +23,38 @@ def main():
             places.append(lines)
 
     places.sort(key=itemgetter(3, 2))  # sort list by visited status then priority
-
     print(len(places), "places loaded from", FILENAME)
-
-    letters_input = ["l", "a", "m", "q"]
-    menu_input = input_menu()
+    letters = ["l", "a", "m", "q"]
+    place_file = open("places.csv", "a+")
+    menu_input = ""
     while menu_input != "q":
+        menu_input = input_menu()
         if menu_input == "l":
             get_max_name_length(places)
-            menu_input = input_menu()
 
         # Get name, country and priority input of a place and add them to the Travel Tracker
         elif menu_input == "a":
-            name_input = input("Name: ")
-            check_name_input(name_input) # Check for blank input
+            name_input = validate_name_input()
+            country_input = validate_country_input()
+            get_priority(name_input, country_input, places, place_file)  # Get priority input and check for
+            # ValueError
 
-            country_input = input("Country: ")
-            check_country_input(country_input)  # Check for blank input
+        elif menu_input == "m":
+            place = [place for place in places]
+            if "n" in place[0][3]:
+                get_max_name_length(places)
+                print("Enter the number of a place to mark as visited")
+                get_number_input(places)
+            else:
+                print("No unvisited places")
 
-            get_priority(name_input, country_input)  # Get priority input and check for ValueError
-
-            menu_input = input_menu()
-
-        # check if the wrong letter has been typed and return the menu if true
-        elif menu_input not in letters_input:
+        elif menu_input not in letters:
             print("Invalid menu choice")
-            menu_input = input_menu()
 
-    print("Have a nice day :)")  # display message when user chooses q
+        else:
+            print(f"{len(places)} places saved in places.csv")
+
+    print("Have a nice day:)")  # display message when user chooses q
 
 
 def input_menu():
@@ -57,8 +62,35 @@ def input_menu():
                        "L - List Places\n"
                        "A - Add new place\n"
                        "M - Mark a place as visited\n"
-                       "Q - Quit").lower()
+                       "Q - Quit\n"
+                       ">>").lower()
     return menu_input
+
+
+def validate_country_input():
+    """Get country input from user and check for blank answers"""
+    country_input = ""
+    validate_input = False
+    while not validate_input:
+        country_input = input("Country: ")
+        if country_input == "":
+            print("Input can not be blank")
+        else:
+            validate_input = True
+    return country_input
+
+
+def validate_name_input():
+    """Get name input from user and check for blank answers"""
+    name_input = ""
+    validate_input = False
+    while not validate_input:
+        name_input = input("Name: ")
+        if name_input == "":
+            print("Input can not be blank")
+        else:
+            validate_input = True
+    return name_input
 
 
 def get_max_name_length(places):
@@ -117,27 +149,14 @@ def display_formatted_list(max_city_length, max_country_length, places):
 
 def display_visit_status(count, unvisited_count):
     """display the number of places visited and not visited"""
-    print(f"{count} places. You still want to visit {unvisited_count} places.")
+    return print(f"{count} places. You still want to visit {unvisited_count} places.")
 
 
-def check_name_input(name_input):
-    """Check if the name_input has any blank input"""
-    while name_input == "":
-        print("Input can not be blank")
-        name_input = input("Name: ")
-
-
-def check_country_input(country_input):
-    """Check if the country_input has any blank input"""
-    while country_input == "":
-        print("Input can not be blank")
-        country_input = input("Country: ")
-
-
-def get_priority(name, country):
+def get_priority(name_input, country_input, places, place_file):
     """Get priority input and validate input
     Display the added place through printing the name,country and priority of the place"""
     validate_input = False
+    priority_input = 0
     while not validate_input:
         try:
             priority_input = int(input("Priority: "))
@@ -145,10 +164,54 @@ def get_priority(name, country):
                 print("Number must be > 0")
             else:
                 validate_input = True
-                print(f"{name} in {country} (priority {priority_input}) added to Travel Tracker")  # display added place
 
         except ValueError:
             print("Invalid input; enter a valid number")
+
+    print(f"{name_input} in {country_input} (priority {priority_input}) added to Travel Tracker")  # display added place
+    new_added_place(name_input, country_input, priority_input, places, place_file)
+
+
+def new_added_place(name, country, priority_input, places, place_file):
+    """Add new place to the nested list of places and sort the list accordingly"""
+    new_place = [name, country, priority_input, NOT_VISITED]
+    places.append(new_place)
+    places.sort(key=itemgetter(3, 2))
+    place_file.write(f"\n{new_place[0]},{new_place[1]},{new_place[2]},{new_place[3]}")
+
+
+def get_number_input(places):
+    number_input = 0
+    validate_input = False
+    while not validate_input:
+        try:
+            number_input = int(input(">>"))
+            if number_input <= 0:
+                print("Number must be > 0")
+
+            elif number_input not in range(1, len(places) + 1):
+                print("Invalid place number")
+
+            else:
+                validate_input = True
+
+        except ValueError:
+            print("Invalid input; enter a valid number")
+    convert_unvisited_place(number_input, places)
+
+
+def convert_unvisited_place(number_input, places):
+    """Convert unvisited place to visited if user marks it as visited"""
+    for count, place in enumerate(places):
+        count += 1
+        while number_input == count:  # checks the number which the user types in that corresponds to a given place
+            if place[3] == "v":
+                print("That place is already visited!")
+            else:
+                print(f"{place[0]} in {place[1]} visited!")
+                place[3] = "v"
+                places.sort(key=itemgetter(3, 2))
+            break
 
 
 main()
